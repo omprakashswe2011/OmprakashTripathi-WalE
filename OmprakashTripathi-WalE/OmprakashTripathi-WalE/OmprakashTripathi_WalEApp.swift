0d6005b97919143d28982b48f957cca90e76b854
Om Prakash Tripathi <opra0002@synchronoss>
2022-11-04 21:14:26 +0530

    Initial commit create empty project

---
+++ OmprakashTripathi-WalE/OmprakashTripathi-WalE/OmprakashTripathi_WalEApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmprakashTripathi_WalEApp.swift
+//  OmprakashTripathi-WalE
+//
+//  Created by Om Prakash Tripathi on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct OmprakashTripathi_WalEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
